6d7d26283569a3aed396d2e2eee7a60903caf837
Moisés Jurado <moises003@gmail.com>
2023-09-11 15:00:00 -0500

    update

--- myapiflask/app.py
+++ myapiflask/app.py
@@ -6,7 +6,8 @@ from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 api = Api(app)
 
-app.config.from_object('myapiflask.config')  # Cambio en la referencia al archivo de configuración
+# Cambio en la referencia al archivo de configuración
+app.config.from_object('myapiflask.config')
 
 # Swagger Configuration
 SWAGGER_URL = '/swagger'
@@ -22,8 +23,3 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 # MongoDB Configuration
 db = MongoEngine(app)
-
-from myapiflask import routes  # Cambio en la referencia al módulo de rutas
-
-if __name__ == '__main__':
-    app.run(debug=True)
