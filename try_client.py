539c6dd18d8ac17a18191c4099ce7c67612a94dc
Moisés Jurado <moises003@gmail.com>
2023-09-25 09:53:56 -0500

    try ok

--- try_client.py
+++ try_client.py
@@ -9,13 +9,14 @@ configuration = swagger_client.Configuration()
 configuration.host = "http://localhost:5000"
 
 # create an instance of the API class
-api_instance = swagger_client.DefaultApi(swagger_client.ApiClient(configuration))
-dni = '1234' # str | 
+api_instance = swagger_client.DefaultApi(
+    swagger_client.ApiClient(configuration))
+DNI = '65'  # str |
 
 try:
     # Eliminar una persona por DNI
-    response=api_instance.get_personas_resource()
-    response=api_instance.delete_persona_resource(dni)
+    response = api_instance.get_personas_resource()
+    response = api_instance.delete_persona_resource(DNI)
     print("hello")
 except ApiException as e:
     print("Exception when calling DefaultApi->delete_persona_resource: %s\n" % e)
